9459a2c5ebda499e38e64ada7cfe9daa599bb26b
Pedro <pedrogoku80@gmail.com>
2024-06-05 17:45:40 -0300

    feat: add to schema in the mongoose

---
+++ src/users/schemas/users.schemas.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+export const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
+
+UserSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    this['password'] = await bcrypt.hash(this['password'], 10);
+  } catch (err) {
+    return next(err);
+  }
+});
